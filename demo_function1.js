b19c5a2ab0926c21056edae9f4b4c7762ff53f73
Jaela Martin <jmartin@appdev.yearup.org>
2023-10-23 15:45:40 -0500

    first commit

---
+++ demo_function1.js
@@ -0,0 +1,28 @@
+// Define the funtion 
+function greet(name,kind){
+
+    return '$(kind)', '$(name)'; // Use b
+
+}
+// This is the function
+function function-name () {
+// code to executed 
+
+}
+// This is the function 
+function displayGreeting () {
+    let message = "Hello world!" ;
+    console.log(message);
+}
+
+// elsewhere...
+// This is a call to the function 
+displayGreeting();
+
+function displayWeekdayMottos() {
+console.log("Make it happen Monday!")
+console.log ("Tackle it Tuseday!")
+console.log(" Finish it Friday!")
+}
+
+displayWeekdayMottos();
---
+++ f_to_c.js
@@ -0,0 +1,10 @@
+// 
+function favoritethings(){
+console.log ("my dog");
+console.log("ReadingBooks");
+}
+function whyImHere(){
+console.log ( "learning to code");
+console.log ("To learn more about computers");
+console.log ("to have a good time");
+}
